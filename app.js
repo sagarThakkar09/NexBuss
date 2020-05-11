f8b4d8eef47ba69cc760860a182cb65a00e6244a
Sagar Thakkar <sthakkar42@gmail.com>
2020-05-11 12:28:22 -0500

    retrieve stop data using street Key

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const formEle = document.querySelector("form");
 let streetEle = document.querySelector(".streets");
+const result = document.querySelector("#street-name");
+
 formEle.addEventListener("submit", function(e) {
     street = e.target.querySelector("input");
     streetSearch(street.value)
@@ -15,20 +17,30 @@ function streetSearch(query) {
                 throw new Error("Something went Wrong!");
             }
         }).then(data => {
-            const streetList = [];
-            streetList.push(data.streets);
-            console.log(streetList);
-            updateStreetList(streetList[0]);
-
+            updateStreetList(data.streets);
         })
 }
 
 function updateStreetList(data) {
     streetEle.innerHTML = ""
     data.forEach(function(street) {
-        streetEle.insertAdjacentHTML(`afterbegin`,
-            ` <a href="#" data-street-key="${street.key}">${street.name}</a>`
-        );
-    })
-
+        streetEle.innerHTML += `<a href="#" data-street-key="${street.key}">${street.name}</a>`
+    });
 };
+
+streetEle.addEventListener("click", function(e) {
+    fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${e.target.dataset.streetKey}&api-key=ZPFv2Zx6ny1KrlPKnfe`)
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+        }).then(data => {
+            getStops(data.stops);
+        })
+})
+
+function getStops(stopList) {
+    stopList.forEach(function(stop) {
+        console.log(stop.key);
+    })
+}
